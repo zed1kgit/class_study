789e67cafe08d5dab4d74036785334129b0c2462
Ilya Seleznev <zedplay.zp@gmail.com>
2024-05-29 17:58:38 +0300

    Added method get_vehicle_type() in class Vehicle

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 class Vehicle:
 
-    def __init__(self):
-        self.name = None
-        self.mileage = None
+    def __init__(self, name, mileage):
+        self.name = name
+        self.mileage = mileage
+
+    def get_vehicle_type(self, wheels):
+        if wheels == "2":
+            vehicle_type = "мотоцикл"
+        elif wheels == "3":
+            vehicle_type = "трицикл"
+        elif wheels == "4":
+            vehicle_type = "автомобиль"
+        else:
+            return "Я не знаю таких ТС"
+        return f"Это {vehicle_type} марки {self.name}"
